8655f48a17b450a61c3b86e3849f567108ace6da
spinnaker <eegrad85@gmail.com>
2018-06-10 20:20:07 -0400

    Hello World v1.0.3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
 <<<<<<< HEAD
-  res.send('Hello World xxx (v1.0.3)!');
+  res.send('Hello World Manny Divina(v1.0.3)!');
 =======
   res.send('Hello World Manny Divina v1.0.3!');
 >>>>>>> c4296b80ba87622c990995b7669b5cc0181ba6c4
