72b9b4fb56732695d7ce3c8877e80e7bfb704332
Manny Divina <edivina@urban.org>
2018-06-12 14:57:28 -0400

    m

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-<<<<<<< HEAD
   res.send('Hello World Manny (v1.0.4)!');
-=======
-  res.send('Hello World Master!');
->>>>>>> 48a5da6c6e8c336863420e8e771cb209493116f0
 });
 
 var server = app.listen(3000, function () {
